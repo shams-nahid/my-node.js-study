4a9e246627d40d57567dd1e180ffa0bfe1c25287
bmshamsnahid <b.m.shamsnahid@gmail.com>
2021-10-28 06:11:13 +0600

    testing the callbacks

---
+++ resource/jest-http/src/test/Launcher.test.ts
@@ -0,0 +1,32 @@
+import { mocked } from 'ts-jest/utils';
+
+import { Launcher } from '../app/Launcher';
+import { Server } from '../app/Server/Server';
+
+jest.mock('../app/Server/Server', () => {
+  return {
+    Server: jest.fn(() => {
+      return {
+        startServer: () => {
+          console.log(`Starting Mock Server`);
+        }
+      };
+    })
+  };
+});
+
+describe('Launcher test suite', () => {
+  const mockedServer = mocked(Server, true);
+
+  test('create server', () => {
+    new Launcher();
+    expect(mockedServer).toBeCalled();
+  });
+
+  test('launch app', () => {
+    const launchAppMock = jest.fn();
+    Launcher.prototype.launchApp = launchAppMock;
+    new Launcher().launchApp();
+    expect(launchAppMock).toBeCalled();
+  });
+});
