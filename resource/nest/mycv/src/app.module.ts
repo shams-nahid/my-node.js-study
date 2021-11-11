e6a6b426b21bf3e2734271adf3c6bcf94d0829bc
bmshamsnahid <b.m.shamsnahid@gmail.com>
2021-11-12 04:11:32 +0600

    added swagger

--- resource/nest/mycv/src/app.module.ts
+++ resource/nest/mycv/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 
--- resource/nest/mycv/src/main.ts
+++ resource/nest/mycv/src/main.ts
@@ -1,14 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // body validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  // Api documentation with swagger
+  const config = new DocumentBuilder()
+    .setTitle('MyCV API Doc')
+    .setDescription('The mycv API description')
+    .setVersion('1.0')
+    .addTag('mycv')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
