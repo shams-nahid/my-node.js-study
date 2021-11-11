84b03cd8497796767131a6521fe958ae942c52f6
bmshamsnahid <b.m.shamsnahid@gmail.com>
2021-11-12 04:23:11 +0600

    added logger

--- resource/nest/mycv/src/users/users.controller.ts
+++ resource/nest/mycv/src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  private readonly logger = new Logger(UsersController.name);
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.logger.log('Getting user data', body);
   }
 }
